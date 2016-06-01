c28a5ce7c4acb26d17b0d65004612651e49d00b4
Matthew Leibowitz <mattleibow@live.com>
2016-06-01 13:42:10 +0200

    Added a sound player for UWP  - doesn't play yet for some reason... might be that m4a is not supported, but the docs say it should work

---
+++ YayMath.UWP/SoundPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Core;
+using Windows.UI.Xaml.Controls;
+
+[assembly: Xamarin.Forms.Dependency(typeof(YayMath.UWP.SoundPlayer))]
+
+namespace YayMath.UWP
+{
+    public class SoundPlayer : ISoundPlayer
+    {
+        MediaElement booSound;
+        MediaElement yaySound;
+
+        public SoundPlayer ()
+        {
+            CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync (CoreDispatcherPriority.Normal, () =>
+            {
+                booSound = new MediaElement { Source = new Uri ("ms-appx:///Assets/Boo.m4a") };
+                yaySound = new MediaElement { Source = new Uri ("ms-appx:///Assets/Yay.m4a") };
+            }).AsTask ();
+        }
+
+        public void PlayBoo ()
+        {
+            booSound?.Play ();
+        }
+
+        public void PlayYay ()
+        {
+            yaySound?.Play ();
+        }
+    }
+}
+
